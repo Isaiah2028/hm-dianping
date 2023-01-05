36b2118c2d1fb1d4630cbeb1652b6b4f42ed6d87
luyuan <luyuan@egoonet.com>
2023-01-05 15:17:46 +0800

    Redis实现分布式锁第一版本改进，释放锁是给线程添加uuid,释放锁时，只能释放自己的锁

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -10,7 +11,7 @@ import java.util.concurrent.TimeUnit;
  * @date: 2023/1/4 22:08
  */
 public class SimpleRedisLock implements ILock {
-    
+
     /**
      * 业务名称，锁名称
      */
@@ -27,6 +28,8 @@ public class SimpleRedisLock implements ILock {
      */
     private static final String KEY_PREFIX = "lock:";
 
+    private static final String ID_PREFIX = UUID.randomUUID(true) + "-";
+
     /**
      * 尝试索取锁
      *
@@ -36,7 +39,7 @@ public class SimpleRedisLock implements ILock {
     @Override
     public boolean tryLock(long timeoutSec) {
         //获取线程标识
-        long threadId = Thread.currentThread().getId();
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         String key = KEY_PREFIX + name;
         Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(key, threadId + "", timeoutSec, TimeUnit.SECONDS);
         //解决success拆箱引起空指针
@@ -48,7 +51,14 @@ public class SimpleRedisLock implements ILock {
      */
     @Override
     public void unlock() {
+        //获取线程表示
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        //获取redis锁中的线程标识
         String key = KEY_PREFIX + name;
-        stringRedisTemplate.delete(key);
+        String threadIdFromRedis = stringRedisTemplate.opsForValue().get(key);
+        if (threadId.equals(threadIdFromRedis)) {
+            stringRedisTemplate.delete(key);
+        }
+
     }
 }
